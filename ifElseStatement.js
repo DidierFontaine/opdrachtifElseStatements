e11e1a9b7275f61905a55adead7eedb6f268241d
DidierFontaine <71550187+DidierFontaine@users.noreply.github.com>
2020-11-10 10:22:44 +0100

    Add files via upload

---
+++ ifElseStatement.js
@@ -0,0 +1,20 @@
+const age = 20;
+if ( age >= 18) {
+    console.log("Jij mag naar binnen");
+} else {
+    console.log("Blijf aub buiten");
+}
+
+const isFemale = false;
+if (isFemale === true) {
+    console.log("Jij bent een vrouw");
+} else {
+    console.log("Jij bent geen vrouw")
+}
+
+const driverStatus = " geen bob";
+if (driverStatus === "bob") {
+    console.log("Jij mag rijden")
+} else {
+    console.log("Jij mag niet rijden")
+}
